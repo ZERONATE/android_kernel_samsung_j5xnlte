1beae47dcc99baaa2d5158e3048d08351662a156
kongwoojin <kongwoojin03@gmail.com>
2018-03-18 17:15:08 +0900

    proc: Add SpeedY kernel version
    Signed-off-by: kongwoojin <kongwoojin03@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,6 +1,7 @@
 
 #!/bin/bash
 export PATH=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin:$PATH
+export SPEEDY_KERNEL_VERSION=0
 mkdir  out
 make -C $(pwd) O=$(pwd)/out ARCH=arm CROSS_COMPILE=arm-eabi- VARIANT_DEFCONFIG=msm8916_sec_j5xlte_skt_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 
